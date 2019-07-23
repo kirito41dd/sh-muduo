b1eb96536a4ce6e73147e81c4409d33c909860c0
kirito <1026860069@qq.com>
2019-07-23 17:58:58 +0800

    大暑真热

---
+++ sh/base/CurrentThread.cpp
@@ -0,0 +1,77 @@
+
+#include "sh/base/CurrentThread.h"
+
+#include <cxxabi.h>
+#include <execinfo.h>
+#include <stdlib.h>
+
+namespace sh
+{
+
+namespace CurrentThread
+{
+
+extern __thread int         t_cachedTid = 0;
+extern __thread char        t_tidString[32];
+extern __thread int         t_tidStringLenght = 6;
+extern __thread const char  *t_threadName = "unknown";
+static_assert(std::is_same<int, pid_t>::value, "pid_t should be int");
+
+string stackTrace(bool demangle)
+{
+    string stack;
+    const int max_frames = 200;
+    void *frame[max_frames];
+    int nptrs = ::backtrace_symbols(frame, max_frames);
+    char **strings = ::backtrace_symbols(frame, nptrs);
+    if (strings)
+    {
+        size_t len = 256;
+        char *demangled = demangle ? static_cast<char*>(::malloc(len)) : nullptr;
+        for (int i = 1; i < nptrs; ++i) // skipping the 0-th, which is this function
+        {
+            if (demangle)
+            {
+                char *left_par = nullptr;
+                char *plus = nullptr;
+                for (char *p = strings[i]; *p; ++p)
+                {
+                    if (*p == '(')
+                        left_par = p;
+                    else if (*p == '+')
+                        plus = p;
+                }
+
+                if (left_par && plus)
+                {
+                    *plus = '\0';
+                    int status = 0;
+                    char * ret = abi::__cxa_demangle(left_par+1, demangled, &len, &status);
+                    *plus = '+';
+                    if (status == 0)
+                    {
+                        demangled = ret; // ret could be realloc()
+                        stack.append(strings[i], left_par+1);
+                        stack.append(demangled);
+                        stack.append(plus);
+                        stack.push_back('\n');
+                        continue;
+                    }
+                }
+            }
+            // fallback to mangled names
+            stack.append(strings[i]);
+            stack.push_back('\n');
+        }
+        free(demangled);
+        free(strings);
+    }
+    return stack;
+}
+
+
+
+
+} // namespace CurrentThread
+
+} // namespace sh
---
+++ sh/base/CurrentThread.h
@@ -0,0 +1,57 @@
+#ifndef SH_BASE_CURRENTTHREAD_H
+#define SH_BASE_CURRENTTHREAD_H
+
+#include "sh/base/Types.h"
+
+namespace sh
+{
+namespace CurrentThread
+{
+    // __thread是GCC内置的线程局部存储设施，存取效率可以和全局变量相比。
+    // __thread变量每一个线程有一份独立实体，各个线程的值互不干扰。
+    // 可以用来修饰那些带有全局性且值可能变，但是又不值得用全局变量保护的变量。
+    // internal
+    extern __thread int         t_cachedTid;
+    extern __thread char        t_tidString[32];
+    extern __thread int         t_tidStringLenght;
+    extern __thread const char  *t_threadName;
+    void cacheTid();
+
+    inline int tid()
+    {
+        // 这个指令是gcc引入的，作用是允许程序员将最有可能执行的分支告诉编译器。
+        // 指令的写法为：__builtin_expect(EXP, N),意思是：EXP==N的概率很大。
+        if (__builtin_expect(t_cachedTid == 0, 0)){
+            cacheTid();
+        }
+        return t_cachedTid;
+    }
+
+    inline const char* tidString() // for logging
+    {
+        return t_tidString;
+    }
+
+    inline int tidStringLength() // for logging
+    {
+        return t_tidStringLenght;
+    }
+
+    inline const char* name()
+    {
+        return t_threadName;
+    }
+
+    bool isMainThread();
+
+    void sleepUsec(int64_t usec); // for testing
+
+    // 将C++源程序标识符(original C++ source identifier)转换成C++ ABI标识符(C++ ABI identifier)的过程
+    // 称为mangle；相反的过程称为demangle。
+    string stackTrace(bool demangle);
+
+} // namespace CurrnetThread
+
+} // namespace sh
+
+#endif // SH_BASE_CURRENTTHREAD_H
